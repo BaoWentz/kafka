d086bc3e225cbdab48d86eac4ba08bd9317c1f30
bwz <549068446@qq.com>
2022-02-15 02:15:36 +0800

    stabel version

---
+++ s1-download_images.sh
@@ -0,0 +1,4 @@
+# Zookeeper
+sudo docker pull wurstmeister/zookeeper
+# Kafka
+sudo docker pull wurstmeister/kafka
---
+++ s2-start_images.sh
@@ -0,0 +1,9 @@
+# docker-compose build
+# 创建并启动容器，可以使用 -f 参数指定docker-compose.yml文件
+sudo docker-compose up -d
+# # 查看状态
+# sudo docker-compose ps
+# # 停止并删除容器，可以使用 -f 参数指定docker-compose.yml文件
+# sudo docker-compose down
+
+# docker exec -it kafka /bin/bash  # 通过容器名称进入到kafka容器中
